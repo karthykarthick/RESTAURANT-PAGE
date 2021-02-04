60dece23b93fafb79e856a61b67fc2674ab88659
karthykarthick <karthylock@gmail.com>
2021-02-04 22:40:12 +0530

    add bg through js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
 console.log("hello");
+console.log("world");
+
+document.body.style.backgroundImage = "url('https://images.pexels.com/photos/6331045/pexels-photo-6331045.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260')";
