2079411ec11f56f35e012e292648d63e58b3bba1
karthykarthick <karthylock@gmail.com>
2021-02-06 02:43:26 +0530

    errors fixed

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,31 @@
-const path = require('path')
+import { resolve } from 'path'
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
-      },
-      {
-        test: /\.(png|svg|jpg|gif|jpeg)$/,
-        use: [
-          'file-loader'
-        ]
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: [
-          'file-loader'
-        ]
-      }
-    ]
-  }
+export const entry = './src/index.js'
+export const output = {
+  filename: 'main.js',
+  // eslint-disable-next-line no-undef
+  path: resolve(__dirname, 'dist')
+}
+export const module = {
+  rules: [
+    {
+      test: /\.css$/,
+      use: [
+        'style-loader',
+        'css-loader'
+      ]
+    },
+    {
+      test: /\.(png|svg|jpg|gif|jpeg)$/,
+      use: [
+        'file-loader'
+      ]
+    },
+    {
+      test: /\.(woff|woff2|eot|ttf|otf)$/,
+      use: [
+        'file-loader'
+      ]
+    }
+  ]
 }
