fe330cca71030cb8d3586f2793c51db0071b4f3b
karthykarthick <karthylock@gmail.com>
2021-02-05 21:41:41 +0530

    added menu page functions

---
+++ src/tabs/main-page.js
@@ -0,0 +1,43 @@
+import menuImage from '../images/menu-img.jpeg';
+
+const menuPage = () =>{
+
+    const section = document.createElement("section");
+    const menuTitle = document.createElement("h1");
+    const container = document.createElement('div');
+    const menuImg = new Image();
+    const menuList = document.createElement("ul");
+    const menuArray = ['Salad', '$8', 'French Fries', '$15', 'Spaghetti', '$14', 'Soup', '$7', 'Pizza', '$14', 'Lasagna', '$13', 'Burguer', '$11'];
+
+
+    const addContent = () => {
+       menuImg.src = menuImage;
+       menuTitle.textContent = "Menu:";
+       container.classList.add("menuContainer");
+       menuTitle.classList.add('title');
+    };
+
+    const appendList = () => {
+        for(let i = 0; i < menuArray.length; i += 1){
+            const listItem = document.createElement('li');
+            listItem.textContent = textContent.menuArray[i];
+            menuList.appendChild(listItem);
+        }
+    };
+
+    const appendElements = () => {
+        section.appendChild(menuImg);
+        container.appendChild(menuTitle);
+        appendList();
+        container.appendChild(menuList);
+        section.appendChild(container);
+    };
+
+    addContent();
+    appendElements();
+
+    return section;
+
+};
+
+export default menuPage;
